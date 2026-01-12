ecd579e0fb0284e514a637789b8ae8f1c0651a02
Gergo Varga <gergoo.varga@gmail.com>
2026-01-12 20:24:10 +0100

    Added test
    added another basic test

--- Flow_Critical_Graphs/src/test/java/AFlowCriticalGraphs.java
+++ Flow_Critical_Graphs/src/test/java/AFlowCriticalGraphs.java
@@ -1,7 +1,6 @@
 import flows.IsCritical;
 import graphs.Graph;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class AFlowCriticalGraphs {
@@ -16,24 +15,37 @@ public class AFlowCriticalGraphs {
     }
 
     @Test
+    public void three_flow(){
+        Graph g = new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(0, 3);
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 3);
+
+        boolean res = IsCritical.is_A_flow_critical(g,3);
+
+        assertTrue(res);
+    }
+
+
+    @Test
     public void petersen(){
         Graph petersen = new Graph(10);
 
-// Vonkajší päťuholník (vrcholy 0-4)
         petersen.addEdge(0, 1);
         petersen.addEdge(1, 2);
         petersen.addEdge(2, 3);
         petersen.addEdge(3, 4);
         petersen.addEdge(4, 0);
 
-// Vnútorná hviezda (vrcholy 5-9)
         petersen.addEdge(5, 7);
         petersen.addEdge(7, 9);
         petersen.addEdge(9, 6);
         petersen.addEdge(6, 8);
         petersen.addEdge(8, 5);
 
-// Prepojenie vonkajšieho a vnútorného cyklu (zodpovedajúce vrcholy)
         petersen.addEdge(0, 5);
         petersen.addEdge(1, 6);
         petersen.addEdge(2, 7);
